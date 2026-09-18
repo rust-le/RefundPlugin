39ccab258d20faf3dc05f501ebfaa6404501d38c
TheMilek <kam.grygierzec@gmail.com>
2026-09-18 14:19:09 +0200

    Load Sylius Behat services from PHP config on Symfony 8 and pin twig below 3.29

--- tests/TestApplication/config/services_test.php
+++ tests/TestApplication/config/services_test.php
@@ -8,7 +8,10 @@ return function (ContainerConfigurator $container) {
     $env = $_ENV['APP_ENV'] ?? 'dev';
 
     if (str_starts_with($env, 'test')) {
-        $container->import('../../../vendor/sylius/sylius/src/Sylius/Behat/Resources/config/services.xml');
+        // Sylius 2.3 ships its Behat services as a PHP config, earlier versions only as XML,
+        // which Symfony 8 can no longer load.
+        $syliusBehatServices = '../../../vendor/sylius/sylius/src/Sylius/Behat/Resources/config/services';
+        $container->import(is_file(__DIR__ . '/' . $syliusBehatServices . '.php') ? $syliusBehatServices . '.php' : $syliusBehatServices . '.xml');
         $container->import('@SyliusRefundPlugin/tests/Behat/Resources/services.php');
     }
 
